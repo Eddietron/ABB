c4ca94965223a7fbd33ab225982ab83088c76121
Servo <tarzanboycraft@hotmail.com>
2018-11-13 18:25:55 -0600

    menu listo

--- app.c
+++ app.c
@@ -5,11 +5,9 @@
 // altura, nivel, minimo, maximo, insertar, modificar nodo, eliminar, mostrar
 
 int capturar(int menu){
-    system("cls");
     switch(menu){
-        case 1:printf("1. Altura \n2. Nivel\n3. Mínimo\n4. Máximo\n5. Insertar\n6. Modificar\n7. Eliminar\n 8. Mostrar\n");break;
+        case 1:printf("1. Altura \n2. Nivel\n3. M%cnimo\n4. M%cximo\n5. Insertar\n6. Modificar\n7. Eliminar\n8. Mostrar\n", 162, 160);break;
         case 2:printf("1. Preorden \n2. Inorden\n3. Postorden\n");break;
-        default: printf("No valido \n");break;
     }
     int numero;
     scanf("%d",&numero);
@@ -22,46 +20,46 @@ void menu(){
     while(1){
         op = capturar(1);
         switch(op){
-            case 1: /*ALTURA*/ break;
-            case 2: /*NIVEL*/ break;
-            case 3: /*MÍNIMO*/ break;
-            case 4: /*MÁXIMO*/ break;
-            case 5: /*INSERTAR*/ break;
-            case 6: /*MODIFICAR*/ break;
-            case 7: /*ELIMINAR*/ break;
+            case 1: printf("altura \n"); break;
+            case 2: printf("nivel \n"); break;
+            case 3: printf("minimo \n"); break;
+            case 4: printf("maximo \n"); break;
+            case 5: printf("insertar \n"); break;
+            case 6: printf("modificar \n"); break;
+            case 7: printf("eliminar \n"); break;
             case 8:
                 op2 = capturar(2);
                 if(op2 == 1){
-                    /*preoreden*/
+                    printf("preorden \n");
                 }else if(op2 == 2){
-                    /*ihnorden*/
+                    printf("inorden \n");
                 }else if(op2 == 3){
-                    /*postorden*/
+                    printf("postorden \n");
                 }
             break;
-            default: break;
+            default: printf("No valido \n");break;
         }
     }
 }
 
 int main(){
 
-    init();
+    //init();
 
     menu();
 
     
-    int a[] = {5,3,4,2};
-    int i = 0;
-    for(;i<4;i++){
-        Nodo* ad = crearNodo(a[i]);
-        insertar(raiz, ad);
-    }
-    inOrden(raiz);
-    printf("\n");
-    eliminarNodo(raiz, 3);
-    inOrden(raiz);
-    printf("\n");
+    // int a[] = {5,3,4,2};
+    // int i = 0;
+    // for(;i<4;i++){
+    //     Nodo* ad = crearNodo(a[i]);
+    //     insertar(raiz, ad);
+    // }
+    // inOrden(raiz);
+    // printf("\n");
+    // eliminarNodo(raiz, 3);
+    // inOrden(raiz);
+    // printf("\n");
 
 
 
